01980dcbdf43ef654c3623e0a0cf0341d7f4ac88
Samir Suthar <samir.suthar90@gmail.com>
2020-03-31 19:54:36 -0600

    traject fixes

--- lab16/traject.py
+++ lab16/traject.py
@@ -5,13 +5,13 @@ from scipy.interpolate import interp1d
 import matplotlib
 import pylab as pl
 
-def freefall(Y,t): return [Y[1],-9.8]
+def freefall(Y,t): return [Y[1],-9.8 - 0.01*Y[1]*np.abs(Y[1])]
 # here, Y = [y,vy], returns [dy/dt, dvy/dt]
 # where dy/dt = vy (by definition in kinematics!)
 # and dvy/dt is acceleration, define here as g = -9.8, grav accel in the lab.
 
 n = 100 # number of time samples
-tmax = 20 # max time
+tmax = 100 # max time
 t = np.linspace(0,tmax,n) # these are the time samples
 y0, v0 = 1000.0, 0.0      # initial conditions
 sol = odeint(freefall, [y0,v0], t) # solve!
@@ -19,10 +19,12 @@ sol = odeint(freefall, [y0,v0], t) # solve!
 
 altfn = interp1d(t,sol[:,0])
 timefn = interp1d(sol[:,0],t)
+
 print(altfn(tmax))
-print('Time to reach altitude of 800m:', timefn(800),'seconds')
+print('Time to reach altitude of 800m:', timefn(800),'seconds, Terminal Velocity:', sol[:,1][-1])
 
 # here on in, just plotting..
+"""
 print(t.shape,sol.shape)
 pl.plot(t,sol[:,0],'-k')
 pl.xlabel('time (s)')
@@ -31,3 +33,4 @@ pl.ylabel('value (a.u.)')
 imfil = 'tmp.png'
 pl.savefig(imfil)
 
+"""
